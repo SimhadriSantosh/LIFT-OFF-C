7edc96d20adafde5459316c44072aa254671ec1c
santoshsimhadri2211 <99494432+santoshsimhadri2211@users.noreply.github.com>
2022-02-11 21:28:14 +0530

    Add files via upload

---
+++ 1.c
@@ -0,0 +1,5 @@
+#include<stdio.h>
+void main(){
+    printf("NAME = Santosh Simhadri\nREGD.NO = 013\nBRANCH = Information Technology(IT)\nHOBBIES = Movies,Coding and playing and following cricket");
+    
+}
